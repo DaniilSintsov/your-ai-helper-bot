df89f937a46c9d153c69d588c536bfcf38f978f2
Daniil Sintsov <daniilpanfilov519@gmail.com>
2023-12-24 22:26:44 +0300

    added the bot template

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello from your-ai-helper-bot');
+import 'dotenv/config';
+import { message } from 'telegraf/filters';
+import { Telegraf } from 'telegraf';
+
+if (!process.env.BOT_TOKEN) {
+	throw new Error('BOT_TOKEN is not defined');
+}
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.on(message('voice'), ctx =>
+	ctx.reply(JSON.stringify(ctx.message.voice, null, 2))
+);
+
+bot.on(message('text'), ctx => ctx.reply(ctx.message.text));
+
+bot.command('start', async ctx => {
+	await ctx.reply(JSON.stringify(ctx.message, null, 2));
+});
+
+bot.launch();
+
+// If the nodejs process has terminated, stop the bot
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
