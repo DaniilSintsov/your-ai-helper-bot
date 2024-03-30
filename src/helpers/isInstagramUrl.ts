af39878189add4d25be6be42e58ed01843e01002
Daniil Sintsov <daniilpanfilov519@gmail.com>
2024-03-30 17:45:33 +0300

    deleted unusable helper function

--- src/helpers/isInstagramUrl.ts
+++
@@ -1,7 +0,0 @@
-export function isInstagramUrl(str: string): boolean {
-	const pattern = new RegExp(
-		'^(?:(?:http|https)?://)?(?:www\\.)?(?:instagram\\.com|instagr\\.am)/([A-Za-z0-9-_]+)',
-		'i',
-	);
-	return !!pattern.test(str);
-}
